5440e8dcdda755ec18e930187174e7642ebd69ea
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-12 14:20:42 +0700

    main

--- exepemdas3_160/exepemdas3_160.cpp
+++ exepemdas3_160/exepemdas3_160.cpp
@@ -7,17 +7,50 @@ public:
 	bidangDatar() { //constructor
 		x = 0;
 	}
-	virtual void input() {} // fungsi yang menerima input dan mengirim input melalui
-	fungsi setX untuk disimpan di x
-		virtual float Luas(int a) { return 0; } //fungsi untuk menghitung luas
-	virtual float Keliling(int a) { return 0; } //fungsi untuk menghitung keliling
+	virtual void input() {
+		
+	} // fungsi yang menerima input dan mengirim input melalui fungsi setX untuk disimpan di x
+	
+		virtual float Luas(int a) { //fungsi untuk menghitung luas
+			return 0; 
+		} 
+
+	virtual float Keliling(int a) {  //fungsi untuk menghitung keliling
+		return 0; 
+	} 
+
 	void setX(int a) { //fungsi untuk memberi/mengirim nilai pada x
 		this->x = a;
 	}
+
 	int getX() { //fungsi untuk membaca/mengambil nilai dalam x
 		return x;
 	}
 };
-class Lingkaran :public bidangDatar { /*lengkapi disini*/ };
+
+
+class Lingkaran :public bidangDatar { 
+private:
+	int jejari;
+public:
+	Lingkaran() {
+		cout << "Lingkaran dibua\n";
+	}
+	void input() {
+		cout << "Masukkan jejari = ";
+		cin >> jejari;
+		setX(jejari);
+	}
+	
+};
 class Bujursangkar :public bidangDatar { /*lengkapi disini*/ };
-int main() { /*lengkapi disini*/ }
+
+int main() { 
+	int setnum;
+	cin >> setnum;
+	Lingkaran L;
+	L.setX(setnum)
+	
+
+	
+}
